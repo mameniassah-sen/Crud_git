c1958f52681b03e6ad00eca6c7e03602c56a8d66
mikalagrace7-coder <mikalagrace7@gmail.com>
2026-03-02 10:31:00 +0000

    grace

--- src/MODELE/Medecin.java
+++ src/MODELE/Medecin.java
@@ -6,5 +6,36 @@ public class Medecin {
 	private String nom;
 	private String sexe; 
 	private String specialite;
+	public int getIdMed() {
+		return idMed;
+	}
+	public void setIdMed(int idMed) {
+		this.idMed = idMed;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getSexe() {
+		return sexe;
+	}
+	public void setSexe(String sexe) {
+		this.sexe = sexe;
+	}
+	public String getSpecialite() {
+		return specialite;
+	}
+	public void setSpecialite(String specialite) {
+		this.specialite = specialite;
+	}
+	
 
 }
